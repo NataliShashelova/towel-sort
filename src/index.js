527d4aed5d6e538932905271350f46d4f93c68a9
NataliShashelova <natalishashelova1993@gmail.com>
2023-01-27 18:18:19 +0100

    feat: add docs tower-sort

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ module.exports = function towelSort(matrix) {
 	if (matrix) {
 		let arr = [];
 		for (let i = 0; i < matrix.length; i++) {
-			if (i % 2 !== 0) {
-				arr.push(matrix[i].reverse());
-			else { arr.push(matrix[i]) };
-			}
+			if (i % 2 !== 0) { arr.push(matrix[i].reverse()); }
+			else { arr.push(matrix[i]); }
 		} return arr.flat();
 	}
 	else { return []; }
